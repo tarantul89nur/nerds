9a0ab351414b9d4d4c8fc6b2c171bc3667ecc87c
tarantul89nur <vickulbakin@mail.ru>
2017-07-06 17:18:43 +0300

    Перенес из форка

---
+++ js/script-min.js
@@ -0,0 +1,3 @@
+var link=document.querySelector(".contacts-btn");var contacts=document.querySelector(".contacts-form");var name=document.querySelector("[name=name]");var close=document.querySelector(".form-btn-close");contacts.classList.add("js-form-hidden");link.addEventListener("click",function(evt){evt.preventDefault();contacts.classList.remove("js-form-hidden");name.focus();})
+close.addEventListener("click",function(evt){evt.preventDefault();contacts.classList.add("js-form-hidden");})
+window.addEventListener("keydown",function(evt){if(evt.keyCode===27){if(contacts.classList.contains("js-form-hidden")){}else{contacts.classList.add("js-form-hidden");}}});
